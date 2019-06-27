3502c699de76265b9c3f0907dff7c67a81690992
s_tomita@actier.co.jp <s_tomita@actier.co.jp>
2019-06-27 12:56:11 +0900

    ヘッダメニューを作成

--- view/src/App.vue
+++ view/src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <HeaderItem></HeaderItem>
     <router-view/>
   </div>
 </template>
 
 <script>
+import TopMenu from "./components/TopMenu"
+import HeaderItem from "./components/HeaderItem"
 export default {
-  name: 'App'
+  name: "App",
+  components: {HeaderItem, TopMenu}
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- view/src/router/index.js
+++ view/src/router/index.js
@@ -1,15 +1,16 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue"
+import Router from "vue-router"
+import TopMenu from "../components/TopMenu"
+import "semantic-ui-css/semantic.min.css"
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: "/",
+      name: "TopMenu",
+      component: TopMenu
     }
   ]
 })
